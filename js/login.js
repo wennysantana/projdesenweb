7c99730788aebc6c94a8366152bc02f028de6bf9
Wenny Santana <101837881+wennysantana@users.noreply.github.com>
2023-10-13 14:30:36 -0300

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,21 @@
+const inputs = document.querySelectorAll(".input");
+
+var form = document.getElementById("formulario");
+form.addEventListener("submit", my_func, true);
+
+function addcl(){
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl(){
+	let parent = this.parentNode.parentNode;
+	if(this.value == ""){
+		parent.classList.remove("focus");
+	}
+}
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
---
+++ js/testecadastro.js
@@ -0,0 +1,4 @@
+function Enviar() {
+    var nome = index.getElementById('aluno')
+    console.log(nome)
+}
